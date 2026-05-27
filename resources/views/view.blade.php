5d694900c91e77d1db8a0c21a0e176f6441bd07e
saswat <saswat@matrixnmedia.com>
2026-05-27 13:05:42 +0530

    Saswat: Fix View

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -50,7 +50,7 @@
                                     <img src="https://cdn.pixabay.com/photo/2017/03/27/16/50/beach-2179624_640.jpg" style="height: 300px;"/>
                                     <br/>
                                     This is a sample art for Facebook Sharing. 
-                                    <a href="https://www.facebook.com/dialog/share?app_id={{env('FACEBOOK_APP_ID')}}&display=popup&href=https://cdn11.bigcommerce.com/s-x49po/products/48245/images/281002/1580321131335_Kshatrani__85751.1687415112.500.659.jpg?c=2&redirect_uri=https://www.fizdi.com">Share on Facebook</a>
+                                    <a href="https://www.facebook.com/dialog/share?app_id={{env('FACEBOOK_APP_ID')}}&display=popup&href=https://media.istockphoto.com/id/1799323584/photo/tropical-landscape-with-beach-with-coconut-trees-at-sunset.jpg?c=2&redirect_uri=https://www.fizdi.com">Share on Facebook</a>
                                 </div>
 
                                 <table style="width: 100%" border="1">
